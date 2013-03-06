9f43dd0510a83f77e3745223fcace888730301d3
Tim Shannon <tim@townsourced.com>
2013-03-06 17:18:53 -0600

    New direction, with unsafe pointer checks instead of self methods.

--- vmath_test.go
+++ vmath_test.go
@@ -5,24 +5,37 @@
 package vmath
 
 import (
-	"github.com/spate/vectormath"
+	"fmt"
+	"github.com/timshannon/vectormath"
 	"testing"
 )
 
 func v3IsEqual(vector *Vector3, other *vectormath.Vector3) bool {
-	if vector[0] != other.X() {
+	if vector[0] != other.X {
 		return false
 	}
 
-	if vector[1] != other.Y() {
+	if vector[1] != other.Y {
 		return false
 	}
 
-	if vector[2] != other.Z() {
+	if vector[2] != other.Z {
 		return false
 	}
 
 	return true
+
+}
+
+func m3IsEqual(t *testing.T, matrix *Matrix3, other *vectormath.Matrix3) {
+
+	for r := 0; r < 3; r++ {
+		for c := 0; c < 3; c++ {
+			if matrix.Elem(c, r) != other.GetElem(c, r) {
+				t.Error("M3 Not equal: ", matrix.Elem(c, r), other.GetElem(c, r))
+			}
+		}
+	}
 
 }
 
@@ -51,5 +64,35 @@ func TestEquality(t *testing.T) {
 	if !v3IsEqual(vStart, sResult) {
 		t.Error("Slerp not equal", vStart, sResult)
 	}
+
+}
+
+func TestM3Equality(t *testing.T) {
+	othermat0 := new(vectormath.Matrix3)
+	othermat1 := new(vectormath.Matrix3)
+	mat0 := &Matrix3{23.41, 21.12, 0,
+		214.23, 213.9821, -32.02,
+		991.90, 75.123, -231.02}
+
+	mat1 := &Matrix3{23.41, 21.12, 0,
+		214.23, 213.9821, -32.02,
+		991.90, 75.123, -231.02}
+
+	for r := 0; r < 3; r++ {
+		for c := 0; c < 3; c++ {
+			//column major
+			othermat0.SetElem(c, r, mat0.Elem(c, r))
+			othermat1.SetElem(c, r, mat1.Elem(c, r))
+		}
+	}
+
+	m3IsEqual(t, mat0, othermat0)
+	m3IsEqual(t, mat1, othermat1)
+
+	vectormath.M3Mul(othermat0, othermat0, othermat1)
+	mat0.Mul(mat0, mat1)
+
+	fmt.Println("After")
+	m3IsEqual(t, mat0, othermat0)
 
 }
